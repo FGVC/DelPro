b81b3ce9f0c02607df2d91cd29f7db2f529ce8f6
Connor Anderson <thecatalystak@gmail.com>
2019-07-29 11:45:02 -0600

    Add dataset for ranked list loss

--- method_code/re-identification/dataset.py
+++ method_code/re-identification/dataset.py
@@ -7,6 +7,24 @@ import json
 import random
 
 
+# Keypoint order
+# 0  lef tear
+# 1  right ear
+# 2  nose
+# 3  right shoulder
+# 4  right front paw
+# 5  left shoulder
+# 6  left front paw
+# 7  right hip
+# 8  right knee
+# 9  right back paw
+# 10 left hip
+# 11 left knee
+# 12 left back paw
+# 13 root of tail
+# 14 center, mid point of 3 and 14
+
+
 T = torchvision.transforms
 totensor = T.Compose([
     T.ToTensor(),
@@ -87,13 +105,23 @@ class _ClassData(torch.utils.data.Dataset):
 
 
 class ReIDDataset(object):
-    def __init__(self, root='data', split='split1'):
+    def __init__(self, root='data', split='reid-hard-splits.split1', color='RGB'):
         self.root = Path(root)
-        splits = json.load(self.root.joinpath('reid-splits.json').open())
+        splitfile, split = split.split('.')
+        splits = json.load(self.root.joinpath(splitfile+'.json').open())
         self.split = splits[split]
         self.entity_files = splits['entities']
 
+        self.keypoints = json.load(
+                self.root.joinpath('reid_keypoints_train.json').open())
+        self.flank_ids = {}
+        for ent in self.split['test']:
+            kp = self.keypoints[self.entity_files[ent][0]]
+            flank = 1 if kp[6] < kp[39] else -1
+            self.flank_ids[ent] = flank
+
         self.preprocess = basic_preprocess()
+        self.color = color
 
     def train(self):
         return _PairwiseReIDData(self, 'train')
@@ -116,10 +144,11 @@ class _ReIDData(torch.utils.data.Dataset):
 
     def __getitem__(self, ind):
         img, lab = self.file_index[ind]
-        img = Image.open(self.data.root / 'train' / img).convert('RGB')
+        img = Image.open(self.data.root / 'train' / img).convert(self.data.color)
         img = self.preprocess(img)
+        flank = self.data.flank_ids[lab]
 
-        return img, int(lab)
+        return img, int(lab), flank
 
 
 class _PairwiseReIDData(_ReIDData):
@@ -137,16 +166,77 @@ class _PairwiseReIDData(_ReIDData):
                 contrast = c2
             cont_lab = anc_lab
         else:
+            # sample a negative example
             cont_lab, c2 = random.sample(self.entities, 2)
             if cont_lab == anc_lab:
                 cont_lab = c2
             flist = self.files[cont_lab]
             contrast = flist[random.randrange(0, len(flist))]
 
-        anchor = Image.open(self.data.root / 'train' / anchor).convert('RGB')
+        root = self.data.root / 'train'
+        anchor = Image.open(root / anchor).convert(self.data.color)
         anchor = self.preprocess(anchor)
-        contrast = Image.open(self.data.root / 'train' / contrast).convert('RGB')
+        contrast = Image.open(root / contrast).convert(self.data.color)
         contrast = self.preprocess(contrast)
 
         return anchor, int(anc_lab), contrast, int(cont_lab)
+
+
+class RankReIDDataset(ReIDDataset):
+    def __init__(self, root='data', split='reid-hard-splits.split1', nc=30,
+                 nimg=3, color='RGB'):
+        super().__init__(root, split, color)
+        self.nc = nc
+        self.nimg = nimg
+
+    def train(self):
+        return _RankReIDData(self, 'train', self.nc, self.nimg)
+
+
+class _RankReIDData(_ReIDData):
+    def __init__(self, data, split, nc=30, nimg=3):
+        super().__init__(data, split)
+        self.nc = nc
+        self.nimg = nimg
+        self.root = self.data.root / 'train'
+        self.sampler = EntitySampler(self, nc, 10)
+        self.collate = concat_collate
+
+    def __len__(self):
+        return len(self.entities)
+
+    def __getitem__(self, ind):
+        lab = self.entities[ind]
+        files = self.files[lab]
+        imgs = random.sample(files, self.nimg)
+        imgs = [Image.open(self.root / i).convert(self.data.color) for i in imgs]
+        imgs = torch.stack([self.preprocess(i) for i in imgs], 0)
+
+        return imgs, [int(lab)] * self.nimg
+
+
+class EntitySampler(torch.utils.data.Sampler):
+    def __init__(self, data_source, n_ents, repeat=1):
+        self.data_source = data_source
+        self.n_ents = n_ents
+        self.repeat = repeat
+
+    def __len__(self):
+        return self.n_ents * self.repeat
+
+    def __iter__(self):
+        return (x for _ in range(self.repeat) for x in
+                random.sample(range(len(self.data_source)), self.n_ents))
+
+
+from torch.utils.data.dataloader import default_collate
+def concat_collate(batch):
+    batch = default_collate(batch)
+    img, label = batch
+    shape = img.size()
+    b, n, q = shape[0], shape[1], shape[2:]
+    img = img.view(b * n, *q)
+    label = torch.stack(label, 1).view(-1)
+
+    return [img, label]
 
